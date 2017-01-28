eb5976a01c67a985936ab26c06e2652b05a23612
Ali Hassan <alihassan19393@gmail.com>
2017-01-28 14:29:06 +0000

    finished the service

--- server.js
+++ server.js
@@ -2,7 +2,13 @@ var express = require('express');
 var app = express();
 
 app.get('/' , function(req , res){
-    res.send('Hello Heroku!');
+    var agent = req.headers['user-agent']
+    var response = {
+        ipaddress: req.headers["x-forwarded-for"],
+        language: req.headers['accept-language'].split(',')[0],
+        software: agent.substring(agent.indexOf('(') + 1 , agent.indexOf(')'))
+    }
+    res.send(JSON.stringify(response));
 })
 
 app.listen(process.env.PORT || 8080);
